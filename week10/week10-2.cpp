0d2e6188dd015796614217cd8abac24f5ca49791
xiang <91250402+marinesea99@users.noreply.github.com>
2021-11-18 17:40:03 +0800

    Add files via upload

---
+++ week10/week10-2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=100;
+    int b=200;
+    int c;
+    printf("a:%d b:%d c:%d\n",a,b,c);
+    ///一次宣告很多個變數
+    int d[3]={100,200,300};
+    printf("d[0]:%d\n",d[0]);
+    printf("d[1]:%d\n",d[1]);
+    printf("d[2]:%d\n",d[2]);
+
+    int f[3];
+    printf("f[0]:%d\n",f[0]);
+    printf("f[1]:%d\n",f[1]);
+    printf("f[2]:%d\n",f[2]);
+}
---
+++ week10/week10-3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+///for(°j°é)°t°}¦CArray
+int main()
+{
+    int d[5]={100,200,300,400,500};
+
+    for(int i=0;i<5;i++){
+        printf("d[%d]:%d\n",i,d[i]);
+    }
+}
